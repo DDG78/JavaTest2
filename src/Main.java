e3925b9f1a3c49b9887eedd51229978b9be28e60
DDG78 <Ddg78@mail.ru>
2023-03-15 22:03:34 +0300

    Cheange src

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int amount = 5000;
         int miles = amount / 20;
+        int bonus = 250;
 
         System.out.println("Итоговый бонус: " + miles);
     }
